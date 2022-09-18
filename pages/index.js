f0b61f96f125b3bd86a99cc04e13ca72ad224108
Mateusbrbza <mateus.c.barboza@gmail.com>
2022-09-18 17:54:18 -0300

    events

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,30 @@
 import React from 'react';
 
-const index = () => {
+import MeetupList from '../components/meetups/MeetupList';
+
+const MEETUPS = [
+  {
+    id: 'm1',
+    title: 'primeiro encontro',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/0/09/Morro_da_Cruz%2C_Florianópolis_-_SC%2C_Brazil_-_panoramio_%28cropped%29.jpg',
+    address: 'morro da cruz, Florianopolis',
+    Description: 'lugar lindo'
+  },
+  {
+    id: 'm2',
+    title: 'segundo encontro',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/f/f5/Catedral_de_florianopolis.JPG',
+    address: 'Praça XV, Florianopolis',
+    Description: 'em frente a catedral'
+  }
+];
+
+const HomePage = () => {
   return (
-    <div>index</div>
+    <MeetupList
+    meetups={MEETUPS}
+    />
   )
 }
 
-export default index;
+export default HomePage;
