6bb317df0b9b2daafc3dab7893dbcbb6c5644a19
Mateusbrbza <mateus.c.barboza@gmail.com>
2022-10-05 19:48:48 -0300

    .

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -2,7 +2,7 @@ import {Fragment} from 'react';
 
 import MeetupDetail from '../../components/meetups/MeetupDetail';
 
-const MeetupDetails = () => {
+function MeetupDetails() {
   return (
     <MeetupDetail 
     image="https://upload.wikimedia.org/wikipedia/commons/0/09/Morro_da_Cruz%2C_Florianópolis_-_SC%2C_Brazil_-_panoramio_%28cropped%29.jpg"
@@ -11,6 +11,44 @@ const MeetupDetails = () => {
     description="descricao da reuniao "
     />
   )
+}
+
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      { 
+        params: {
+          meetupId: 'm1',
+      },
+    },
+    { 
+        params: {
+          meetupId: 'm2',
+      },
+    },
+    ]
+  }
+}
+
+export async function getStaticProps(context) {
+  // fetch data for a single meetup
+  const meetupId = context.params.meetupId;
+
+  console.log(meetupId);
+
+  return {
+    props: {
+      meetupData: {
+        image: 
+        "https://upload.wikimedia.org/wikipedia/commons/0/09/Morro_da_Cruz%2C_Florianópolis_-_SC%2C_Brazil_-_panoramio_%28cropped%29.jpg",
+        id: 'meetupId',
+        title: 'primeira reuniao',
+        address: 'florinaopolis, flroianopliis',
+        description: 'descricao da reuniao',
+      }
+    }
+  }
 }
 
 export default MeetupDetails;
