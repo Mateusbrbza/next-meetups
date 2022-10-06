8b26fa187b2bc38c14692f4d83c0f147e0e5f6ed
Mateusbrbza <mateus.c.barboza@gmail.com>
2022-10-05 23:04:45 -0300

    working with MongoDB

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -5,7 +5,7 @@ import {MongoClient} from 'mongodb';
 
 async function handler(req, res) {
     if (req.method === 'POST') {
-        const dat = req.body;
+        const data = req.body;
 
         const {title, image, address, description} = data;
 
